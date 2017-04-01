42c558099e5489da088b71949ccc66ccd422efbd
pkmohan50 <pkmohan50@users.noreply.github.com>
2017-04-01 11:59:01 +0530

    Update aapp.js

--- Work on this/AIRTRACK 3/aapp.js
+++ Work on this/AIRTRACK 3/aapp.js
@@ -21,10 +21,11 @@
 				var k = keys[i];
 				var ContactNo = Amenities[k].ContactNo;
 				var Description = Amenities[k].Description;
+				var imgUrl = Amenities[k].Logo;
 				console.log(ContactNo, Description);
 				document
 					.querySelector("#amenitieslist")
-					.innerHTML+='<li class="list-group-item Amenities[k]" style="border-style:solid; text-align:center; border-color:Teal; text-decoration:none;border-width:10px; font-size:30px; margin:20px;  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24); position:relative; width:500;  "><div><img style="text-align:riht;" src="https://s15.postimg.org/aq8a956t7/462084.jpg" width="150" height="150"><p style="pos "><p class="lead">'+k+'</p><p>'+Amenities[k].ContactNo+'</p><p>'+Amenities[k].Description+'</p></div></li>';
+					.innerHTML+='<li class="list-group-item Amenities[k]" style="border-style:solid; text-align:center; border-color:Teal; text-decoration:none;border-width:10px; font-size:30px; margin:20px;  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24); position:relative; width:500;  "><div><img style="text-align:riht;" src="'+imgUrl+'" width="150" height="150"><p style="pos "><p class="lead">'+k+'</p><p>'+Amenities[k].ContactNo+'</p><p>'+Amenities[k].Description+'</p></div></li>';
 			}
 		}
 
